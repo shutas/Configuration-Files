9958650920534d5693846dfaa3427898c9cc4ff3
しゅーたす <shutas@umich.edu>
2018-10-06 12:24:57 -0400

    ADD: myBash.sh

---
+++ myBash.sh
@@ -0,0 +1,9 @@
+# Install Vundle package manager
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install linters needed for Vim Ale
+pip install autopep8
+pip install pycodestyle
+pip install pylint
+
+
